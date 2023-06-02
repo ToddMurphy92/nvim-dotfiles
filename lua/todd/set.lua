91fa9fa028a7a2e527721888284a7266e9e7514a
Todd Murphy <admin@toddmurphy.me>
2023-06-02 22:32:29 +1000

    Uncommented line that disabled the Q binding

--- lua/todd/set.lua
+++ lua/todd/set.lua
@@ -2,5 +2,5 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
--- Unmap Q
-vim.keymap.set("n", "Q", "<nop>")
+-- Unmap Q -- Uncommented as neovim has changed the default behaviour it seems
+-- vim.keymap.set("n", "Q", "<nop>")
