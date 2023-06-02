ae324f77cae3b7080453bbc35669238139f3122b
Todd Murphy <admin@toddmurphy.me>
2023-06-02 23:31:00 +1000

    Finished moving all config to smaller more manageable files

--- lua/todd/set.lua
+++ lua/todd/set.lua
@@ -4,3 +4,9 @@ vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
 -- Unmap Q -- Uncommented as neovim has changed the default behaviour it seems
 -- vim.keymap.set("n", "Q", "<nop>")
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
